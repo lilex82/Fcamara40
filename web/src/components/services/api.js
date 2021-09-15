b11cfdec81d94e068e2a47915a06201076289f88
Lila <lilex82@gmail.com>
2021-09-15 02:06:22 -0300

    Adicionando pasta Services arquivo api.js com URL do backend

--- web/src/components/services/api.js
+++ web/src/components/services/api.js
@@ -1,14 +1,1 @@
-const URL_API = 'http://localhost:3001'
- 
-export const createBooking = (booking) => {
-  return axios.post(`${URL_API}/book`, booking).then(data => data.response)
-}
- 
-export const checkAvailability = (officeId, date) => {
-  return axios.get(`${URL_API}/offices/${officeId}?date=${date}`).then(data => data.response)
-}
- 
-export const getOffices = () => {
-  return axios.get(`${URL_API}/offices`).then(data => data.response)
-}
- 
+
