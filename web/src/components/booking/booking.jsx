e1e3f0617f7d639d2bb507a77ca53a7485a11659
maisawr <maisawres@gmail.com>
2021-09-16 00:21:09 -0300

    Criada página para confirmar a reserva

---
+++ web/src/components/booking/booking.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import {
+    BoxContainer,
+    FormContainer,
+    Input,
+    SubmitButton,
+  } from "../accountBox/common";
+import { Marginer } from "../marginer";
+import * as S from "./index";
+
+export default function ConfirmInformation(props) {
+    //let date = new URLSearchParams(window.location.search).get('date')
+    const { office } = useParams();
+    const { date } = useParams();
+    const { time } = useParams();
+    const { name } = useParams();
+    const { lastName } = useParams();
+    const { email } = useParams();
+    const history = useHistory();
+    const goBack = () => {
+      history.goBack()
+    }
+
+    function confirm() {
+      //Integração com api/banco de dados
+      //axios.post
+
+      history.push('bookinginformation'); 
+    }
+
+    return (
+        <S.AppContainer>
+          <S.BoxContainer>
+            <S.TopContainer>
+              <S.BackDrop />
+              <S.HeaderContainer>
+                <S.HeaderText>Sistema de</S.HeaderText>
+                <S.HeaderText>Agendamento</S.HeaderText>
+              </S.HeaderContainer>
+            </S.TopContainer>
+            <S.InnerContainer>
+              <BoxContainer>
+              <S.SmallText>Confirme seus dados</S.SmallText>
+                <div>
+                  <div>
+                    <small>Escritório</small>
+                    <small>Data</small>
+                    <small>Horário</small>
+                    <small>Estação</small>
+                    <small>Nome</small>
+                    <small>E-mail</small>
+                  </div>
+                  <div>
+                    <small>{office}</small>
+                    <small>{date}</small>
+                    <small>{time}</small>
+                    <small></small>
+                    <small>{`${name} ${lastName}`}</small>
+                    <small>{email}</small>
+                  </div>
+                </div>
+                <Marginer direction="vertical" margin="1em" />
+                <Marginer direction="vertical" margin="1,5em" />
+                <div>
+                  <small>Cuidados</small>
+                  <small>O uso de máscara é obrigatório</small>
+                  <small>Mantenha o distanciamento social (2m)</small>
+                </div>
+                <SubmitButton type="submit" onClick={confirm}>Continuar</SubmitButton>
+                <SubmitButton type="submit" onClick={goBack}>Voltar</SubmitButton>
+              </BoxContainer>
+            </S.InnerContainer>
+          </S.BoxContainer>
+        </S.AppContainer>
+    );
+}
