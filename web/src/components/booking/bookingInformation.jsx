60fcc0d4d3bf8730158505d582fbbf880d92297c
maisawr <maisawres@gmail.com>
2021-09-16 00:19:53 -0300

    Criada página para mostrar informações da reserva

---
+++ web/src/components/booking/bookingInformation.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import {
+    BoxContainer,
+    FormContainer,
+    Input,
+    SubmitButton,
+  } from "../accountBox/common";
+import { Marginer } from "../marginer";
+import * as S from "./index";
+
+export default function ShowInformation(props) {
+
+  //Integração com api/banco de dados
+  //axios.get
+
+    return (
+        <S.AppContainer>
+          <S.BoxContainer>
+            <S.TopContainer>
+              <S.BackDrop />
+              <S.HeaderContainer>
+                <S.HeaderText>Sistema de</S.HeaderText>
+                <S.HeaderText>Agendamento</S.HeaderText>
+              </S.HeaderContainer>
+            </S.TopContainer>
+            <S.InnerContainer>
+              <BoxContainer>
+              <S.SmallText>Seu agendamento foi realizado com sucesso</S.SmallText>
+                <table>
+                    <tr>Data </tr>
+                    <tr>Horário </tr>
+                    <tr>Estação de trabalho</tr>
+                </table>
+                <Marginer direction="vertical" margin="1em" />
+                <Marginer direction="vertical" margin="1,5em" />
+                <table>
+                    <thead>Cuidados</thead>
+                    <tr>O uso de máscara é obrigatório</tr>
+                    <tr>Leve seu álcool em gel</tr>
+                    <tr>Mantenha o distanciamento social</tr>
+                </table>
+              </BoxContainer>
+            </S.InnerContainer>
+          </S.BoxContainer>
+        </S.AppContainer>
+    );
+}
