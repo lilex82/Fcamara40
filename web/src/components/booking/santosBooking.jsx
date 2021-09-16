72f007151bdd2ae87ac1a08469309673d972ea59
maisawr <maisawres@gmail.com>
2021-09-16 00:43:34 -0300

    Atualizadas rotas

--- web/src/components/booking/santosBooking.jsx
+++ web/src/components/booking/santosBooking.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 
-export default function AgendamentoSantos(props) {
+export default function BookingSantos(props) {
     return (
         <h2>FCamara Escritório Santos</h2>
     );
